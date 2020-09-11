e57c326a7fe9e76dd545161a3dd6a0b5004508b8
ArhumMalik3 <ArhumMalik95@gmail.com>
2020-09-11 08:59:10 -0500

    finished 2

--- Linq Problems/Program.cs
+++ Linq Problems/Program.cs
@@ -10,6 +10,23 @@ namespace Linq_Problems
     {
         static void Main(string[] args)
         {
+            List<string> words = new problems().GetWords();
+            var wordsWithTH = words.Where(w => w.Contains("th"));
+            foreach (var word in wordsWithTH)
+            {
+                Console.WriteLine(word);
+            }
+
+            //Using LINQ, write a function that takes in a list of strings and returns a copy of the list without duplicates.
+            //        List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
+
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
+            var unduplicatedNames = names.Distinct();
+            foreach(var name in unduplicatedNames)
+            {
+                Console.WriteLine(name);
+            }
+
         }
     }
 }
---
+++ Linq Problems/problems.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Problems
+{
+    class problems
+    {
+        //number 1 Using LINQ, write a function that returns all words that contain the substring “th” from a list .
+        //List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", “mathematics” };
+
+        
+        
+        public List<string> GetWords()
+        {
+            return new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+        }
+
+
+    }
+}
